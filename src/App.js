3c48b385cc2080d2d2bfb495f193c370d4eb5c23
Fame <natthapol3011@gmail>
2019-09-12 12:35:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
   state = {}
   render() {
     return (
       <div>
-        
+        <h1>Hello World</h1>
       </div>
     );
   }
---
+++ src/AppBar/AppBar.js
@@ -0,0 +1,12 @@
+import react, { Component } from "react";
+
+class AppBar extends Component {
+    state = {  }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default AppBar;
